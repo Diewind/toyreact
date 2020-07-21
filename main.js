1effba71e8124dc0a2e745b4d903b2ac8c0f1f86
diewind <1290334299@qq.com>
2020-07-21 22:40:55 +0800

    优化功能

--- main.js
+++ main.js
@@ -1,23 +1,51 @@
 import ToyReact,{Component} from './ToyReact';
 
-class MyComponent extends Component{
-    render(){
+class Square extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            value:null
+        }
+    }
+    render() {
         return (
-            <div>
-                <span>hello</span>
-                <p>toyreact</p>
-                <div>
-                    {false}
-                    {this.children}
-                </div>
-            </div>
+            <button className="square" style='width:20px;height:20px;' onClick={()=>{this.setState({value:'x'})}}>
+                {this.state.value ? this.state.value : ""}
+            </button>
         )
     }
 }
-
-const App = (
-    <MyComponent name="toy" id="toyreact">
-      <div>hello world</div>
-    </MyComponent>
-  );
-ToyReact.render(App, document.body);
+  
+class Board extends Component {
+    renderSquare(i) {
+      return (
+        <Square
+          value={i}
+        />
+      );
+    }
+  
+    render() {
+      return (
+        <div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+}
+const app = <Board />;
+ToyReact.render(app, document.body);
